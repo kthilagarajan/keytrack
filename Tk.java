6a84945f3c9485c32c2202551aa27af4de5b8e63
Thilak <kthilagarajan@gmail.com>
2016-04-26 12:25:45 +0530

    Package Removed

--- Tk.java
+++ Tk.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaapplication2;
 
 import java.io.BufferedWriter;
 import java.io.File;
